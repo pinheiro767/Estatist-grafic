077637b4f2623f72247853ff2399c14e5f692143
pinheiro767 <claudiapinheiro767@gmail.com>
2026-03-29 22:30:12 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,7 @@
+<script>
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("service-worker.js")
+      .then(() => console.log("Service Worker registrado"))
+      .catch(err => console.log("Erro:", err));
+  }
+</script>
